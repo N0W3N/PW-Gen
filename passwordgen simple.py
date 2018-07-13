302f5b775be1cd1c3c9f57d3217bdb27ef340843
N0W3N <richynh93@gmail.com>
2018-07-14 00:40:04 +0200

    Create passwordgen simple.py

---
+++ passwordgen simple.py
@@ -0,0 +1,27 @@
+import string
+import secrets
+
+def password_char():
+    charset = string.ascii_letters + string.digits + '!"§$%&/()=?´^{[]}\/+#-.,;:_*/<>'
+
+    return charset
+
+def generate_pw(charset):
+    password = []
+
+    for c in range(40):
+        password += secrets.choice(charset)
+
+    pw_new = ''.join(password)
+    print(pw_new)
+    return pw_new
+
+def main():
+    stub = password_char()
+    generate_pw(stub)
+
+if __name__ == "__main__":
+    main()
+
+
+
